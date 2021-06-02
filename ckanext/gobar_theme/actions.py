ddd3fe06e7814b0c7c0524f1d3fdd2ae0f96fa88
Gaston Jouglard <gastonjouglard.10@gmail.com>
2021-06-02 14:28:44 -0300

    Check active datastore

--- ckanext/gobar_theme/actions.py
+++ ckanext/gobar_theme/actions.py
@@ -33,7 +33,9 @@ def _resource_purge(context, data_dict):
 
 def _resource_delete_from_datastore(context, data_dict):
     id = logic.get_or_bust(data_dict, 'id')
-    ckanext.datastore.logic.action.datastore_delete(context, {'resource_id': id, 'force': True})
+    datastore_active = logic.get_or_bust(data_dict, 'datastore_active')
+    if(datastore_active)
+        ckanext.datastore.logic.action.datastore_delete(context, {'resource_id': id, 'force': True})
 
 
 def resource_delete_and_purge(context, data_dict):
